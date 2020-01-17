ec5951c1aceb6364fce97aaf38d2d5f1c77b7a4c
TimothyADavis <tadavisphd@gmail.com>
2020-01-17 17:36:15 +0000

    version 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
  
 setup(name='gastimator',
-       version='0.1.9',
+       version='0.2.0',
        description='Implementation of a Python MCMC gibbs-sampler with adaptive stepping',
        url='https://github.com/TimothyADavis/GAStimator',
        author='Timothy A. Davis',
